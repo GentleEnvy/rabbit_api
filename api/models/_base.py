1452bb3b47f070dc88b4aa931986617c7dbe850b
Envy <envy15@mail.ru>
2021-06-26 00:04:46 +0300

    fix abstract = True into Meta BaseModel class

--- api/models/_base.py
+++ api/models/_base.py
@@ -4,6 +4,9 @@ __all__ = ['BaseModel']
 
 
 class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
