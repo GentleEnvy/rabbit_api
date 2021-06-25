7a5812a4697e7230c88032916203264ced69dad6
Envy <envy15@mail.ru>
2021-06-25 23:31:49 +0300

    add rabbit serializers, path and views

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,38 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('rabbit/<int:id>/', views.RabbitView.as_view(), name='rabbit_url'),
-    path('rabbit/', views.RabbitView.as_view(), name='rabbits_url'),
-    path('rabbit/bunny/<int:id>/', views.MotherRabbitView.as_view(), name='bunny_url'),
-    path('rabbit/bunny/<int:id>/', views.MotherRabbitView.as_view(), name='bunny_url'),
-    path('rabbit/mother/<int:id>/', views.MotherRabbitView.as_view(), name='mother_url'),
-    path('rabbit/bunny/<int:id>/', views.MotherRabbitView.as_view(), name='bunny_url')
+    path(
+        'rabbit/<int:id>/', views.RabbitView.as_view(),
+        name='rabbit_url'
+    ),
+    path(
+        'rabbit/dead/<int:id>/', views.DeadRabbitView.as_view(),
+        name='dead_rabbit_url'
+    ),
+
+    path('rabbit/bunny/<int:id>/', views.BunnyView.as_view(),
+         name='bunny_url'),
+    path(
+        'rabbit/bunny/create/', views.BunnyCreateView.as_view(),
+        name='bunny_create_url'
+    ),
+
+    path(
+        'rabbit/fattening/<int:id>/', views.FatherRabbitView.as_view(),
+        name='fattening_rabbit_url'
+    ),
+
+    path(
+        'rabbit/mother/<int:id>/', views.MotherRabbitView.as_view(),
+        name='mother_rabbit_url'
+    ),
+    path(
+        'rabbit/mother/create/', views.MotherRabbitCreateView.as_view(),
+        name='mother_create_url'
+    ),
+
+    path(
+        'rabbit/father/<int:id>/', views.FatherRabbitView.as_view(),
+        name='father_rabbit_url'
+    )
 ]
--- api/views.py
+++ api/views.py
@@ -1,33 +1,61 @@
 from django.shortcuts import redirect
-from django.urls import reverse
-from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.views import APIView, exception_handler
+from rest_framework.generics import CreateAPIView
 
 from api.models import *
-from api.serializers import RabbitSerializer, MotherRabbitSerializer
+from api.serializers import *
 
 
+def _get_by_id(rabbit_class, rabbit_serializer, request, id):
+    rabbit = rabbit_class.objects.get(id=id)
+    serializer = rabbit_serializer(rabbit, context={'request': request})
+    return Response(serializer.data)
+
+
+# noinspection PyMethodMayBeStatic
 class RabbitView(APIView):
-    def get(self, request, id=None):
-        if id is None:
-            rabbits = Rabbit.objects.all()
-            serializer = RabbitSerializer(rabbits, many=True)
-            return Response(serializer.data)
-        else:
+    def get(self, request, id):
+        try:
             rabbit = Rabbit.objects.get(id=id)
-            return redirect(reverse('rabbit_url') + )
-
-    def post(self, request: Request):
-        print(request.data)
-        rabbit = Bunny(**request.data)
-        rabbit.full_clean()
-        rabbit.save()
-        return redirect(reverse('rabbit_url') + str(rabbit.id))
+        except Exception as e:
+            return exception_handler(e, {'request', request})
+        return redirect(rabbit.get_absolute_url())
 
 
+# noinspection PyMethodMayBeStatic
+class DeadRabbitView(APIView):
+    def get(self, request, id):
+        return _get_by_id(DeadRabbit, DeadRabbitSerializer, request, id)
+
+
+# noinspection PyMethodMayBeStatic
+class FatteningRabbitView(APIView):
+    def get(self, request, id):
+        return _get_by_id(FatteningRabbit, FatteningRabbitSerializer, request, id)
+
+
+# noinspection PyMethodMayBeStatic
+class BunnyView(APIView):
+    def get(self, request, id):
+        return _get_by_id(Bunny, BunnySerializer, request, id)
+
+
+class BunnyCreateView(CreateAPIView):
+    serializer_class = BunnySerializer
+
+
+# noinspection PyMethodMayBeStatic
 class MotherRabbitView(APIView):
     def get(self, request, id):
-        mother_rabbits = MotherRabbit.objects.get(id=id)
-        serializer = MotherRabbitSerializer(mother_rabbits, context={'request': request})
-        return Response(serializer.data)
+        return _get_by_id(MotherRabbit, MotherRabbitSerializer, request, id)
+
+
+class MotherRabbitCreateView(CreateAPIView):
+    serializer_class = MotherRabbitSerializer
+
+
+# noinspection PyMethodMayBeStatic
+class FatherRabbitView(APIView):
+    def get(self, request, id):
+        return _get_by_id(FatherRabbit, FatherRabbitSerializer, request, id)
