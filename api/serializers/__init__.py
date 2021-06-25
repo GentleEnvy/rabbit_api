5cd33c4296b82951ba18f6f69790ae9dec5ca42b
Envy <envy15@mail.ru>
2021-06-26 00:05:05 +0300

    create BaseModelSerializer

---
+++ api/serializers/__init__.py
@@ -0,0 +1,1 @@
+from api.serializers._rabbit import *
---
+++ api/serializers/_base.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from rest_framework.utils import model_meta
+
+__all__ = ['BaseModelSerializer']
+
+
+class BaseModelSerializer(serializers.ModelSerializer):
+    def get_field_names(self, declared_fields, info):
+        field_names = super().get_field_names(declared_fields, info)
+        if hasattr(self, 'Meta'):
+            model_field_name_dicts = model_meta.get_field_info(self.Meta.model)[1:]
+            model_field_names = set()
+            for model_field_name_dict in model_field_name_dicts:
+                model_field_names |= model_field_name_dict.keys()
+            if (request := self.context.get('request')) is not None:
+                for param_field, is_access_str in request.query_params.items():
+                    try:
+                        is_access = bool(int(is_access_str))
+                    except ValueError:
+                        continue
+                    if is_access and param_field in model_field_names:
+                        field_names.append(param_field)
+                    elif param_field in field_names:
+                        field_names.remove(param_field)
+        return field_names
