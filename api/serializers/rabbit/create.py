32b19803a74349ebe715ef9364e611a71bc68ccf
Envy <envy15@mail.ru>
2021-06-26 18:07:55 +0300

    mark current_type as hidden filed

--- api/serializers/rabbit/create.py
+++ api/serializers/rabbit/create.py
@@ -1,3 +1,5 @@
+from rest_framework.serializers import HiddenField, BooleanField, ModelSerializer
+
 from api.serializers._base import BaseModelSerializer
 from api.models import *
 
@@ -12,10 +14,7 @@ class FatteningRabbitCreateSerializer(BaseModelSerializer):
         model = FatteningRabbit
         fields = '__all__'
 
-    def create(self, validated_data):
-        return super().create(validated_data | {
-            'current_type': FatteningRabbit.TYPE_FATTENING
-        })
+    current_type = HiddenField(default=FatteningRabbit.CHAR_TYPE)
 
 
 class BunnyCreateSerializer(BaseModelSerializer):
@@ -23,14 +22,22 @@ class BunnyCreateSerializer(BaseModelSerializer):
         model = Bunny
         fields = '__all__'
 
+    current_type = HiddenField(default=Bunny.CHAR_TYPE)
+
 
 class MotherRabbitCreateSerializer(BaseModelSerializer):
     class Meta:
         model = MotherRabbit
         fields = '__all__'
 
+    current_type = HiddenField(default=MotherRabbit.CHAR_TYPE)
+    is_male = HiddenField(default=False)
+
 
 class FatherRabbitCreateSerializer(BaseModelSerializer):
     class Meta:
         model = FatherRabbit
         fields = '__all__'
+
+    current_type = HiddenField(default=FatherRabbit.CHAR_TYPE)
+    is_male = HiddenField(default=True)
--- api/views/rabbit/create.py
+++ api/views/rabbit/create.py
@@ -3,6 +3,7 @@ from urllib.parse import urlencode
 from django.core.exceptions import MultipleObjectsReturned, FieldDoesNotExist
 from django.shortcuts import redirect
 from rest_framework.generics import CreateAPIView
+from rest_framework import filters
 
 from api.serializers.rabbit.create import *
 
@@ -12,7 +13,7 @@ __all__ = [
 ]
 
 
-class RedirectCreateView(CreateAPIView):
+class _RedirectCreateView(CreateAPIView):
     def _get_url_by_pk(self, pk):
         model = self.serializer_class.Meta.model
         try:
@@ -23,7 +24,7 @@ class RedirectCreateView(CreateAPIView):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         if response.status_code == 201:
-            if pk := response.data.get('pk'):
+            if pk := (response.data.get('pk') or response.data.get('id')):
                 if url := self._get_url_by_pk(pk):
                     if query_params := request.query_params:
                         return redirect(f'{url}?{urlencode(query_params)}')
@@ -31,17 +32,17 @@ class RedirectCreateView(CreateAPIView):
         return response
 
 
-class FatteningRabbitCreateView(RedirectCreateView):
+class FatteningRabbitCreateView(_RedirectCreateView):
     serializer_class = FatteningRabbitCreateSerializer
 
 
-class BunnyCreateView(CreateAPIView):
+class BunnyCreateView(_RedirectCreateView):
     serializer_class = BunnyCreateSerializer
 
 
-class MotherRabbitCreateView(CreateAPIView):
+class MotherRabbitCreateView(_RedirectCreateView):
     serializer_class = MotherRabbitCreateSerializer
 
 
-class FatherRabbitCreateView(CreateAPIView):
+class FatherRabbitCreateView(_RedirectCreateView):
     serializer_class = FatherRabbitCreateSerializer
