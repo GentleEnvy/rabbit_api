97b86777b8a3ce0b5c4109a4293865831895e1b6
Envy <envy15@mail.ru>
2021-06-29 11:23:23 +0300

    add ListenDiffModel

--- api/models/base.py
+++ api/models/base.py
@@ -2,6 +2,8 @@ from django.db import models
 
 __all__ = ['BaseModel']
 
+from django.forms import model_to_dict
+
 
 class BaseModel(models.Model):
     class Meta:
@@ -10,3 +12,39 @@ class BaseModel(models.Model):
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
+
+
+class ListenDiffModel(BaseModel):
+    class Meta:
+        abstract = True
+
+    listening_fields: tuple[str] = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__real = self.__listening_dict
+
+    @property
+    def diff(self):
+        real_dict = self.__real
+        listening_dict = self.__listening_dict
+        diffs = {
+            field: new_value
+            for field, new_value in listening_dict.items()
+            if new_value != real_dict[field]
+        }
+        return diffs
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.__real = self.__listening_dict
+
+    @property
+    def __listening_fields(self) -> tuple[str]:
+        if self.listening_fields == '__all__':
+            return tuple(self._meta.fields)
+        return self.listening_fields
+
+    @property
+    def __listening_dict(self):
+        return model_to_dict(self, fields=self.__listening_fields)
