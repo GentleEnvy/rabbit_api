c8d54b728be283f414d73dd06465c3a86da682c1
Envy <envy15@mail.ru>
2021-06-30 15:24:49 +0300

    fix rabbit serializer: mother and father are no longer serialized

--- api/views/model_views/rabbit/_default_serializers.py
+++ api/views/model_views/rabbit/_default_serializers.py
@@ -1,21 +1,27 @@
 from rest_framework.serializers import *
 
+from api.models import Cage
 from api.serializers.base import BaseModelSerializer
 
 __all__ = ['create_default_retrieve_serializer', 'create_default_create_serializer']
 
 
 def create_default_retrieve_serializer(
-        serializer_model, serializer_depth=0, is_male_field=None
+        serializer_model, is_male_field=None
 ):
+    class CageSerializer(BaseModelSerializer):
+        class Meta:
+            model = Cage
+            fields = '__all__'
+
     class DefaultRetrieveSerializer(BaseModelSerializer):
         class Meta:
             model = serializer_model
             fields = '__all__'
-            depth = serializer_depth
 
         current_type = CharField(read_only=True)
         is_male = is_male_field
+        cage = CageSerializer()
 
     return DefaultRetrieveSerializer
 
