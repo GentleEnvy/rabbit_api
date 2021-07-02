671ea000ecb2edbec3a9e520f11f3292beb5de95
Envy <envy15@mail.ru>
2021-07-02 18:52:13 +0300

    make the status of a fattening rabbit computable

--- api/managers/rabbit/_time_manager.py
+++ api/managers/rabbit/_time_manager.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 from datetime import datetime, timedelta
 
+from django.utils import timezone
+
 from api.managers.base import *
 from api.models import *
 
@@ -26,18 +28,49 @@ class RabbitTimeManager(BaseTimeManager):
 class FatteningRabbitTimeManager(RabbitTimeManager):
     model: 'FatteningRabbit'
 
-    STATUS_NEED_VACCINATED = 'NV'
+    STATUS_NEED_VACCINATION = 'NV'
     STATUS_NEED_INSPECTION = 'NI'
-    STATUS_FATTENING_SLAUGHTER = 'FS'  # FIXME: leave ?
+    # noinspection SpellCheckingInspection
+    STATUS_WITHOUT_COCCIDIOSTATIC = 'WC'
+    STATUS_READY_TO_SLAUGHTER = 'RS'
 
     @property
     def status(self):
-        statuses = set()
         if not self.model.is_vaccinated:
-            return statuses.add(self.STATUS_NEED_VACCINATED)
-        if self.age.days >= 80:
-            return statuses.add(self.STATUS_NEED_INSPECTION)
-        return statuses or {self.STATUS_FATTENING_SLAUGHTER}
+            return {self.STATUS_NEED_VACCINATION}
+        # vaccinated
+        if (age := self.age.days) >= 80:
+            try:
+                last_inspection = Inspection.objects.filter(
+                    time__gte=self.model.birthdate + timedelta(80),
+                    rabbit=self.model
+                ).latest('time')
+            except Inspection.DoesNotExist:
+                return {self.STATUS_NEED_INSPECTION}
+            # recently there was an inspection
+            if last_inspection.delay is None:
+                try:
+                    last_inspection_with_delay = Inspection.objects.filter(
+                        time__gte=self.model.birthdate + timedelta(80),
+                        rabbit=self.model
+                    ).exclude(delay=None).latest('time')
+                except Inspection.DoesNotExist:
+                    # not underweight
+                    if age >= 90:
+                        return {self.STATUS_READY_TO_SLAUGHTER}
+                    return {self.STATUS_WITHOUT_COCCIDIOSTATIC}
+                # was underweight
+                if last_inspection_with_delay.time + timedelta(
+                        last_inspection_with_delay.delay + 10
+                ) > timezone.now():
+                    return {self.STATUS_READY_TO_SLAUGHTER}
+                return {self.STATUS_WITHOUT_COCCIDIOSTATIC}
+            # is underweight
+            if last_inspection.time + timedelta(last_inspection.delay) > timezone.now():
+                return {self.STATUS_NEED_INSPECTION}
+            # continue to fattening on delay
+        # younger than 80 days
+        return set()
 
 
 class BunnyTimeManager(RabbitTimeManager):
@@ -74,11 +107,10 @@ class FatherRabbitTimeManager(RabbitTimeManager):
     model: 'FatherRabbit'
 
     STATUS_RESTING = 'R'
-    STATUS_MATING = 'M'  # FIXME: leave ?
 
     @property
     def status(self):
         rabbits_in_cage = self.model.cage.cast.rabbits
         if len(rabbits_in_cage) == 1:
             return {self.STATUS_RESTING}
-        return {self.STATUS_MATING}
+        return set()
