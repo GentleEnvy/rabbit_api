246a22dd432b08838e4346a4e3e50eef3c6d70ae
Envy <envy15@mail.ru>
2021-07-05 00:09:10 +0300

    add PregnancyInspection

--- api/models/_inspections.py
+++ api/models/_inspections.py
@@ -4,11 +4,17 @@ from django.utils import timezone
 from api.models._rabbits import *
 from api.models.base import *
 
-__all__ = ['Inspection']
+__all__ = ['BeforeSlaughterInspection', 'PregnancyInspection']
 
 
-class Inspection(BaseModel):
+class BeforeSlaughterInspection(BaseModel):
     rabbit = models.ForeignKey(Rabbit, on_delete=models.CASCADE)
     time = models.DateField(default=timezone.now)
     weight = models.FloatField()
     delay = models.IntegerField(null=True, blank=True)
+
+
+class PregnancyInspection(BaseModel):
+    mother_rabbit = models.ForeignKey(MotherRabbit, on_delete=models.CASCADE)
+    time = models.DateField(default=timezone.now)
+    is_pregnant = models.BooleanField()
