109232a4de51251b997d85d0f09ef770da2f2ecc
Envy <envy15@mail.ru>
2021-07-10 22:47:39 +0300

    fix VaccinationOperation

--- api/views/opertaion.py
+++ api/views/opertaion.py
@@ -1,7 +1,5 @@
-from datetime import datetime
 from typing import Any
 
-from dateutil.parser import parse
 from rest_framework.request import Request
 from rest_framework.response import Response
 
