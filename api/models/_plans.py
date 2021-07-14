3f92b167a5bcff3bd31561530ae8e0b9b38d5e13
Envy <envy15@mail.ru>
2021-07-14 18:16:22 +0300

    update model Plan (+ migration)

--- api/models/_plans.py
+++ api/models/_plans.py
@@ -3,17 +3,17 @@ from django.utils import timezone
 
 from api.models.base import BaseModel
 
-__all__ = ['Plan', 'SlaughterPlan', 'MatingPlan']
+__all__ = ['Plan']
 
 
 class Plan(BaseModel):
+    type = models.CharField(
+        choices=(
+            (SLAUGHTER := 'S', 'SLAUGHTER'),
+            (MATING := 'M', 'MATING')
+        ),
+        max_length=1
+    )
     date = models.DateField(default=timezone.now)
+    quantity = models.PositiveSmallIntegerField()
     is_completed = models.BooleanField(default=False)
-
-
-class SlaughterPlan(Plan):
-    number_rabbits = models.PositiveSmallIntegerField()
-
-
-class MatingPlan(Plan):
-    number_pairs = models.PositiveSmallIntegerField()
