ba775c54979a7b2f8b426005105958eb4d58b933
Envy <envy15@mail.ru>
2021-07-17 21:32:43 +0300

    update Plan and add fk<FatteningRabbit.plan -> Plan> (+ migration)

--- api/models/_plans.py
+++ api/models/_plans.py
@@ -6,14 +6,8 @@ from api.models.base import BaseModel
 __all__ = ['Plan']
 
 
+# TODO: add validation:
+#   - fatteningrabbit_set.count() <= quantity
 class Plan(BaseModel):
-    type = models.CharField(
-        choices=(
-            (SLAUGHTER := 'S', 'SLAUGHTER'),
-            (MATING := 'M', 'MATING')
-        ),
-        max_length=1
-    )
     date = models.DateField(default=timezone.now)
     quantity = models.PositiveSmallIntegerField()
-    is_completed = models.BooleanField(default=False)
