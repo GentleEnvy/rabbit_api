575ae052cdb0aa5f40bf8ff33e8cb96eaabead77
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2021-07-27 01:14:36 +0300

    add mother feeds accounting

--- api/models/_feeds.py
+++ api/models/_feeds.py
@@ -4,15 +4,19 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from datetime import date
 
+from django.utils import timezone
+
 from api.models.base import BaseModel
 
 __all__ = ['FeedBatch']
 
 
 class FeedBatch(BaseModel):
-    delivery_date = models.DateField(default=date.today())
-    total_bags_number = models.IntegerField()
-    bags_left = models.IntegerField()
+    delivery_date = models.DateField(default=timezone.now)
+    total_common_bags_number = models.IntegerField(default=0)
+    common_bags_left = models.IntegerField(default=0)
+    total_mother_bags_number = models.IntegerField(default=0)
+    mother_bags_left = models.IntegerField(default=0)
 
     def clean(self):
         super().clean()
--- api/services/feeding_service.py
+++ api/services/feeding_service.py
@@ -6,8 +6,8 @@ from api.models import FeedBatch
 
 class FeedingService:
     FEED_BAG_WEIGHT = 25000
-    NORMAL_DAILY_CONSUMPTION = 250
-    INCREASED_DAILY_CONSUMPTION = 300
+    NORMAL_DAILY_CONSUMPTION = 200
+    INCREASED_DAILY_CONSUMPTION = 250
     DAYS_FOR_PROGNOSTICATION = 14
     
     def __init__(
@@ -15,7 +15,7 @@ class FeedingService:
         feed_bag_weight=FEED_BAG_WEIGHT,
         normal_daily_consumption=NORMAL_DAILY_CONSUMPTION,
         increased_daily_consumption=INCREASED_DAILY_CONSUMPTION,
-        days_for_prognostication=DAYS_FOR_PROGNOSTICATION
+        days_for_prognostication=DAYS_FOR_PROGNOSTICATION,
     ):
         self.feed_bag_weight = feed_bag_weight
         self.normal_daily_consumption = normal_daily_consumption
@@ -23,14 +23,26 @@ class FeedingService:
         self.days_for_plan = days_for_prognostication
     
     def next_delivery_date(self):
-        feeding_rabbits_amount = Rabbit.objects.filter(current_type__in=(
-            FatteningRabbit.CHAR_TYPE, FatherRabbit.CHAR_TYPE, MotherRabbit.CHAR_TYPE
+        common_rabbits_amount = Rabbit.objects.filter(current_type__in=(
+            Rabbit.TYPE_FATHER, Rabbit.TYPE_FATTENING
         )).count()
-        days_left = FeedBatch.objects.last().bags_left * self.feed_bag_weight // (
-            feeding_rabbits_amount *
+        for mother in MotherRabbit.objects.all():
+            if 'FB' not in mother.manager.status:
+                common_rabbits_amount += 1
+        feeding_mothers_amount = 0
+        for mother in MotherRabbit.objects.all():
+            if 'FB' in mother.manager.status:
+                feeding_mothers_amount += 1
+
+        days_left_common = FeedBatch.objects.last().common_bags_left * self.feed_bag_weight // (
+            common_rabbits_amount *
             self.normal_daily_consumption
         )
-        return date.today() + timedelta(days_left)
+        days_left_mother = FeedBatch.objects.last().mother_bags_left * self.feed_bag_weight // (
+            feeding_mothers_amount *
+            self.normal_daily_consumption
+        )
+        return date.today() + timedelta(days_left_common if days_left_common < days_left_mother else days_left_mother)
     
     # ToDo: calculate amount of feeding rabbits for each day using prognosis
     def rabbits_with_prognosis(self) -> list:
