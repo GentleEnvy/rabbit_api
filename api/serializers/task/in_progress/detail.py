cbc3629cf450faf6aedd79739c7bb2258594e7b6
Envy <envy15@mail.ru>
2021-07-26 18:16:03 +0300

    add InProgressBunnyJiggingTaskUpdateSerializer and InProgressSlaughterInspectionTaskUpdateSerializer

--- api/serializers/task/in_progress/detail.py
+++ api/serializers/task/in_progress/detail.py
@@ -2,21 +2,43 @@ from datetime import datetime
 
 from rest_framework import serializers
 
-from api.models import Task
+from api.models import *
 
-__all__ = ['InProgressTaskUpdateSerializer']
+__all__ = [
+    'InProgressTaskUpdateSerializer', 'InProgressBunnyJiggingTaskUpdateSerializer',
+    'InProgressSlaughterInspectionTaskUpdateSerializer'
+]
 
 
 class InProgressTaskUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['completed_at', 'males', 'females']
-        extra_kwargs = {'completed_at': {'required': False, 'default': datetime.now}}
+        fields = ['completed_at']
+        extra_kwargs = {
+            'completed_at': {
+                'required': False, 'allow_null': False, 'default': datetime.now
+            }
+        }
+
+
+class InProgressBunnyJiggingTaskUpdateSerializer(InProgressTaskUpdateSerializer):
+    class Meta(InProgressTaskUpdateSerializer.Meta):
+        model = BunnyJiggingTask
+        fields = InProgressTaskUpdateSerializer.Meta.fields + ['males', 'females']
+        extra_kwargs = InProgressTaskUpdateSerializer.Meta.extra_kwargs | {
+            'males': {'required': True, 'allow_null': False},
+            'females': {'required': True, 'allow_null': False}
+        }
+
+
+class InProgressSlaughterInspectionTaskUpdateSerializer(InProgressTaskUpdateSerializer):
+    class Meta(InProgressTaskUpdateSerializer.Meta):
+        model = SlaughterInspectionTask
+        fields = InProgressTaskUpdateSerializer.Meta.fields + ['weights']
+        extra_kwargs = InProgressTaskUpdateSerializer.Meta.extra_kwargs | {
+            'weights': {'required': True, 'allow_null': False}
+        }
     
-    males = serializers.IntegerField(required=False, allow_null=False)
-    females = serializers.IntegerField(required=False, allow_null=False)
-    
-    weights = serializers.ListField(
-        child=serializers.IntegerField(), required=False, allow_null=False,
-        allow_empty=False
-    )
+    def validate_weights(self, weights):
+        self.Meta.model.clean_weights(weights)
+        return weights
--- api/views/model_views/task/in_progress/detail.py
+++ api/views/model_views/task/in_progress/detail.py
@@ -1,13 +1,31 @@
-from api.models import Task
-from api.serializers import InProgressTaskUpdateSerializer
+from api.models import *
+from api.serializers.task.in_progress import *
 from api.services.controllers.task.base import TaskController
+from api.services.controllers.task import *
 from api.views.model_views.base import BaseDetailView
 
-__all__ = ['InProgressTaskDetailView']
+__all__ = [
+    'InProgressTaskDetailView', 'InProgressBunnyJiggingTaskDetailView',
+    'InProgressSlaughterInspectionTaskDetailView'
+]
 
 
 class InProgressTaskDetailView(BaseDetailView):
     model = Task
-    queryset = TaskController().in_progress.select_subclasses()
+    queryset = TaskController().in_progress.select_subclasses().filter(
+        bunnyjiggingtask=None, slaughterinspectiontask=None
+    )
     lookup_url_kwarg = 'id'
     update_serializer = InProgressTaskUpdateSerializer
+
+
+class InProgressBunnyJiggingTaskDetailView(InProgressTaskDetailView):
+    model = BunnyJiggingTask
+    queryset = BunnyJiggingTaskController().in_progress
+    update_serializer = InProgressBunnyJiggingTaskUpdateSerializer
+
+
+class InProgressSlaughterInspectionTaskDetailView(InProgressTaskDetailView):
+    model = SlaughterInspectionTask
+    queryset = SlaughterInspectionTaskController().in_progress
+    update_serializer = InProgressSlaughterInspectionTaskUpdateSerializer
