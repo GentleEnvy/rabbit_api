1c17dfc1e925d9a567601697ea0de74ee96d6311
Envy <envy15@mail.ru>
2021-07-27 13:42:07 +0300

    fix feeding service

--- api/services/feeding.py
+++ api/services/feeding.py
@@ -1,5 +1,6 @@
 from datetime import date, timedelta
 
+from api.managers import MotherRabbitManager
 from api.models import *
 from api.models import FeedBatch
 
@@ -23,28 +24,28 @@ class FeedingService:
         self.days_for_plan = days_for_prognostication
     
     def next_delivery_date(self):
-        common_rabbits_amount = Rabbit.objects.filter(current_type__in=(
-            Rabbit.TYPE_FATHER, Rabbit.TYPE_FATTENING
-        )).count()
-        for mother in MotherRabbit.objects.all():
-            if 'FB' not in mother.manager.status:
-                common_rabbits_amount += 1
+        common_rabbits_amount = Rabbit.objects.filter(
+            current_type__in=(Rabbit.TYPE_FATHER, Rabbit.TYPE_FATTENING)
+        ).count()
         feeding_mothers_amount = 0
-        for mother in MotherRabbit.objects.all():
-            if 'FB' in mother.manager.status:
+        for mother in MotherRabbit.all_current():
+            if MotherRabbitManager.STATUS_FEEDS_BUNNY not in mother.manager.status:
+                common_rabbits_amount += 1
+            else:  # FEEDS_BUNNY not in mother status
                 feeding_mothers_amount += 1
-
-        days_left_common = FeedBatch.objects.last().common_bags_left * self.feed_bag_weight // (
-            common_rabbits_amount *
-            self.normal_daily_consumption
+        
+        last_feed_batch = FeedBatch.objects.last()
+        days_left_common = last_feed_batch.common_bags_left * self.feed_bag_weight // (
+            common_rabbits_amount * self.normal_daily_consumption
         )
-        days_left_mother = FeedBatch.objects.last().mother_bags_left * self.feed_bag_weight // (
-            feeding_mothers_amount *
-            self.normal_daily_consumption
+        days_left_mother = last_feed_batch.mother_bags_left * self.feed_bag_weight // (
+            feeding_mothers_amount * self.normal_daily_consumption
         )
-        return date.today() + timedelta(days_left_common if days_left_common < days_left_mother else days_left_mother)
+        return date.today() + timedelta(
+            days_left_common if days_left_common < days_left_mother else days_left_mother
+        )
     
-    # ToDo: calculate amount of feeding rabbits for each day using prognosis
+    # TODO: calculate amount of feeding rabbits for each day using prognosis
     def rabbits_with_prognosis(self) -> list:
         rabbits_for_each_day = []
         for day in range(self.days_for_plan):
