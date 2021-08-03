b36572ec60d4bada6b95a01d2c1bf2f9b89cd1c8
Envy <envy15@mail.ru>
2021-08-04 00:43:35 +0300

    modify echo

--- api/views/echo.py
+++ api/views/echo.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from api.logs import critical
 from api.views.base import BaseView
 
 __all__ = ['EchoView']
@@ -8,14 +8,30 @@ __all__ = ['EchoView']
 
 # noinspection PyMethodMayBeStatic
 class EchoView(BaseView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     
     def get(self, request, *args, **kwargs):
+        return self._echo(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self._echo(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self._echo(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        return self._echo(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self._echo(request, *args, **kwargs)
+    
+    def _echo(self, request, *args, **kwargs):
         return Response(
             {
                 'GET': request.GET,
                 'POST': request.POST,
                 'data': request.data,
+                'query_params': request.query_params,
                 'user': str(request.user),
                 'auth': str(request.auth),
                 'args': args,
