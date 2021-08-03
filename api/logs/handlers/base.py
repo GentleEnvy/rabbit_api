abaf1aa3ad5077e98d2450d1046333d5fad8aa1a
Envy <envy15@mail.ru>
2021-08-03 20:23:35 +0300

    add log handlers

---
+++ api/logs/handlers/base.py
@@ -0,0 +1,18 @@
+from logging import Handler
+
+from api.logs._drainer import LogsDrainer
+
+__all__ = ['BaseHandler']
+
+
+class BaseHandler(Handler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._logs_drainer = LogsDrainer(
+            path_to_logs_file='api/logs/logs.log',
+            directory_to_upload='/rabbit/api/logs'
+        )
+    
+    def handle(self, record):
+        self._logs_drainer.check()
+        return super().handle(record)
