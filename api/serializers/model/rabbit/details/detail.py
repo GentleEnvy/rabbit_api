999a36d5b5ae952a34174bbd59ba22a89c0e7014
Envy <envy15@mail.ru>
2021-08-10 11:36:04 +0300

    add RabbitTypeField

--- api/serializers/model/rabbit/details/detail.py
+++ api/serializers/model/rabbit/details/detail.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
 from api.serializers.base import BaseReadOnlyRaiseSerializer
-from api.serializers.model.rabbit.base import TypedRabbitSerializerMixin
+from api.serializers.fields.rabbit import RabbitTypeField
 from api.models import *
 
 __all__ = [
@@ -17,13 +19,11 @@ class _CageSerializer(serializers.ModelSerializer):
 
 
 # noinspection PyMethodMayBeStatic
-class FatteningRabbitDetailSerializer(
-    TypedRabbitSerializerMixin, BaseReadOnlyRaiseSerializer
-):
-    class Meta(TypedRabbitSerializerMixin.Meta):
-        model = FatteningRabbit
-        read_only_fields = TypedRabbitSerializerMixin.Meta.fields + [
-            'id', 'is_male', 'birthday', 'breed', 'cage', 'status'
+class _BaseRabbitDetailSerializer(BaseReadOnlyRaiseSerializer):
+    class Meta:
+        read_only_fields = [
+            'id', 'current_type', 'is_male', 'birthday', 'breed', 'cage', 'status',
+            'last_weighting'
         ]
         fields = read_only_fields + ['weight']
         depth = 1
@@ -31,58 +31,39 @@ class FatteningRabbitDetailSerializer(
     cage = _CageSerializer(read_only=True)
     status = serializers.SerializerMethodField()
     breed = serializers.SerializerMethodField()
+    current_type = RabbitTypeField()
     
     def get_status(self, rabbit):
         return rabbit.cast.manager.status
     
     def get_breed(self, rabbit):
         return rabbit.breed.title
+    
+    def save(self, **kwargs):
+        self.validated_data['last_weighting'] = datetime.utcnow()
+        super().save()
 
 
-# noinspection PyMethodMayBeStatic
-class BunnyDetailSerializer(TypedRabbitSerializerMixin, BaseReadOnlyRaiseSerializer):
-    class Meta(TypedRabbitSerializerMixin.Meta):
+class FatteningRabbitDetailSerializer(_BaseRabbitDetailSerializer):
+    class Meta(_BaseRabbitDetailSerializer.Meta):
         model = FatteningRabbit
-        read_only_fields = TypedRabbitSerializerMixin.Meta.fields + [
-            'id', 'is_male', 'birthday', 'breed', 'cage', 'status'
-        ]
-        fields = read_only_fields + ['weight']
-        depth = 1
-    
-    cage = _CageSerializer(read_only=True)
-    status = serializers.SerializerMethodField()
-    breed = serializers.SerializerMethodField()
-    
-    def get_status(self, rabbit):
-        return rabbit.cast.manager.status
-    
-    def get_breed(self, rabbit):
-        return rabbit.breed.title
+
+
+class BunnyDetailSerializer(_BaseRabbitDetailSerializer):
+    class Meta(_BaseRabbitDetailSerializer.Meta):
+        model = Bunny
 
 
 # noinspection PyMethodMayBeStatic
-class _ReproductionRabbitDetailSerializer(
-    TypedRabbitSerializerMixin, BaseReadOnlyRaiseSerializer
-):
-    class Meta(TypedRabbitSerializerMixin.Meta):
-        read_only_fields = TypedRabbitSerializerMixin.Meta.fields + [
-            'id', 'is_male', 'birthday', 'breed', 'cage', 'status', 'output',
-            'output_efficiency'
+class _ReproductionRabbitDetailSerializer(_BaseRabbitDetailSerializer):
+    class Meta(_BaseRabbitDetailSerializer.Meta):
+        read_only_fields = _BaseRabbitDetailSerializer.Meta.read_only_fields + [
+            'output', 'output_efficiency'
         ]
         fields = read_only_fields + ['weight']
-        depth = 1
     
-    cage = _CageSerializer(read_only=True)
-    status = serializers.SerializerMethodField()
-    breed = serializers.SerializerMethodField()
     output = serializers.SerializerMethodField()
     output_efficiency = serializers.SerializerMethodField()
-    
-    def get_status(self, rabbit):
-        return rabbit.cast.manager.status
-    
-    def get_breed(self, rabbit):
-        return rabbit.breed.title
     
     def get_output(self, rabbit):
         return rabbit.manager.output
--- api/serializers/model/rabbit/generals/general.py
+++ api/serializers/model/rabbit/generals/general.py
@@ -2,7 +2,7 @@ from django.forms import model_to_dict
 from rest_framework import serializers
 
 from api.serializers.base import BaseSupportsCageSerializer
-from api.serializers.model.rabbit.base import TypedRabbitSerializerMixin
+from api.serializers.fields.rabbit import RabbitTypeField
 from api.models import *
 
 __all__ = [
@@ -12,16 +12,18 @@ __all__ = [
 
 
 # noinspection PyMethodMayBeStatic
-class RabbitListSerializer(TypedRabbitSerializerMixin, serializers.ModelSerializer):
-    class Meta(TypedRabbitSerializerMixin.Meta):
+class RabbitListSerializer(serializers.ModelSerializer):
+    class Meta:
         model = Rabbit
-        fields = TypedRabbitSerializerMixin.Meta.fields + [
-            'id', 'cage', 'birthday', 'is_male', 'breed', 'weight', 'status'
+        fields = [
+            'id', 'cage', 'birthday', 'is_male', 'breed', 'weight', 'status',
+            'current_type'
         ]
     
     cage = serializers.SerializerMethodField()
     status = serializers.SerializerMethodField()
     breed = serializers.SerializerMethodField()
+    current_type = RabbitTypeField()
     
     def get_cage(self, rabbit):
         return model_to_dict(rabbit.cast.cage, fields=['farm_number', 'number', 'letter'])
@@ -33,27 +35,22 @@ class RabbitListSerializer(TypedRabbitSerializerMixin, serializers.ModelSerializ
         return rabbit.breed.title
 
 
-class MotherRabbitCreateSerializer(
-    TypedRabbitSerializerMixin, BaseSupportsCageSerializer
-):
-    class Meta(TypedRabbitSerializerMixin.Meta):
+class _BaseReproductionRabbitCreateSerializer(BaseSupportsCageSerializer):
+    class Meta:
+        fields = ['birthday', 'breed', 'cage', 'is_male', 'is_vaccinated']
+    
+    is_vaccinated = serializers.HiddenField(default=True)
+
+
+class MotherRabbitCreateSerializer(_BaseReproductionRabbitCreateSerializer):
+    class Meta(_BaseReproductionRabbitCreateSerializer.Meta):
         model = MotherRabbit
-        fields = TypedRabbitSerializerMixin.Meta.fields + [
-            'birthday', 'breed', 'cage', 'is_male', 'is_vaccinated'
-        ]
     
     is_male = serializers.HiddenField(default=False)
-    is_vaccinated = serializers.HiddenField(default=True)
 
 
-class FatherRabbitCreateSerializer(
-    TypedRabbitSerializerMixin, BaseSupportsCageSerializer
-):
-    class Meta(TypedRabbitSerializerMixin.Meta):
+class FatherRabbitCreateSerializer(BaseSupportsCageSerializer):
+    class Meta(_BaseReproductionRabbitCreateSerializer.Meta):
         model = FatherRabbit
-        fields = TypedRabbitSerializerMixin.Meta.fields + [
-            'birthday', 'breed', 'cage', 'is_male', 'is_vaccinated'
-        ]
     
     is_male = serializers.HiddenField(default=True)
-    is_vaccinated = serializers.HiddenField(default=True)
