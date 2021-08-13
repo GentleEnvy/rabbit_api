36895004c1c5483347970e8decaaa4d1c45945b9
Envy <envy15@mail.ru>
2021-08-13 16:28:51 +0300

    override AnonymousTaskDetailView.perform_update(): add an individual task update when assigning a performer

--- api/views/model/task/anonymous/detail.py
+++ api/views/model/task/anonymous/detail.py
@@ -1,5 +1,6 @@
 from api.models import Task
 from api.serializers.model.task.anonymous.detail import AnonymousTaskUpdateSerializer
+from api.services.model.task.controllers import all_controllers
 from api.services.model.task.controllers.base import TaskController
 from api.views.model.base import BaseDetailView
 
@@ -11,3 +12,10 @@ class AnonymousTaskDetailView(BaseDetailView):
     queryset = TaskController().anonymous.all()
     lookup_url_kwarg = 'id'
     update_serializer = AnonymousTaskUpdateSerializer
+    
+    def perform_update(self, serializer):
+        super().perform_update(serializer)
+        task = serializer.instance
+        for controller_class in all_controllers:
+            if isinstance(task, controller_class.task_model):
+                controller_class().setup(task)
