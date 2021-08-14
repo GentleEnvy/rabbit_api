2fdc2f2c115f7e06a986860ab69f66f02616aed0
Envy <envy15@mail.ru>
2021-08-14 15:39:06 +0300

    modify CreateBunnyJiggingTask

--- api/tests/model/task/anonymous/test_create.py
+++ api/tests/model/task/anonymous/test_create.py
@@ -1,7 +1,6 @@
 from unittest import mock
 
 from django.test import TestCase
-from freezegun import freeze_time
 
 from api.models import *
 from api.services.model.task.controllers import *
@@ -12,19 +11,16 @@ fp = 'model/task/anonymous/create'
 class CreateBunnyJiggingTask(TestCase):
     fixtures = ['init/breed', f'{fp}/bunny_jigging']
     
-    # @freeze_time("2021-02-15")
     def test__suc(self):
-        with mock.Mock(
+        with mock.patch(
             'api.services.model.rabbit.managers._manager.BunnyManager.status',
-            return_value={Bunny.Manager.STATUS_NEED_JIGGING}
-        ) as m:
-            print(m)
+            mock.PropertyMock(return_value={Bunny.Manager.STATUS_NEED_JIGGING})
+        ):
             BunnyJiggingTaskController().update_anonymous()
         task = Task.objects.select_subclasses().get()
         self.assertEqual(task.CHAR_TYPE, BunnyJiggingTask.CHAR_TYPE)
         self.assertEqual(task.cage_from, MotherCage.objects.get())
     
-    @freeze_time("2021-02-14")
     def test__err(self):
         BunnyJiggingTaskController().update_anonymous()
         self.assertEqual(Task.objects.count(), 0)
