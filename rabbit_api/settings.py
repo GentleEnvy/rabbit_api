14c0563abcd0c85b4ddd45490a09a5f440eab2a9
Envy <envy15@mail.ru>
2021-08-15 11:35:49 +0300

    add TEST setting

--- rabbit_api/settings.py
+++ rabbit_api/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-DEBUG = False if (_debug := os.environ.get('DJANGO_DEBUG')) is None else bool(int(_debug))
+DEBUG = False if (_debug := os.getenv('DJANGO_DEBUG')) is None else bool(int(_debug))
 
 ALLOWED_HOSTS = ['*']
 
@@ -115,21 +115,6 @@ FIXTURE_DIRS = ['api/tests/fixtures/']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if DEBUG:  # dev
-    LOGGING = LogConfig(
-        {
-            'api': {'handlers': [api_console]},
-            'django.server': {'handlers': [web_console]}
-        }
-    ).to_dict()
-else:  # prod
-    LOGGING = LogConfig(
-        {
-            'api': {'handlers': [api_file, api_console, email_admins]},
-            'django.server': {'handlers': [web_file, web_console]}
-        }
-    ).to_dict()
-
 ADMINS = [('envy', 'envy15@mail.ru'), ('envy', 'komarov.sergei163@gmail.com')]
 
 EMAIL_HOST = 'smtp.gmail.com'
@@ -141,7 +126,30 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 ###
 # Custom
 
+TEST = False if (_test := os.getenv('DJANGO_TEST')) is None else bool(int(_test))
 YANDEX_DISK_TOKEN = os.getenv('YANDEX_DISK_TOKEN')
 
 # Custom
 ###
+
+if DEBUG:  # dev
+    LOGGING = LogConfig(
+        {
+            'api': {'handlers': [api_console]},
+            'django.server': {'handlers': [web_console]}
+        }
+    ).to_dict()
+elif TEST:  # test
+    LOGGING = LogConfig(
+        {
+            'api': {'handlers': [api_file, api_console]},
+            'django.server': {'handlers': [web_file, web_console]}
+        }
+    ).to_dict()
+else:  # prod
+    LOGGING = LogConfig(
+        {
+            'api': {'handlers': [api_file, api_console, email_admins]},
+            'django.server': {'handlers': [web_file, web_console]}
+        }
+    ).to_dict()
