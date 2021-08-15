03be6231d4896607d8217441be87a146cff235ea
Envy <envy15@mail.ru>
2021-08-15 21:02:24 +0300

    fix NotEmptyResponseMiddleware

--- api/middlewares/_not_empty_response.py
+++ api/middlewares/_not_empty_response.py
@@ -8,7 +8,10 @@ __all__ = ['NotEmptyResponseMiddleware']
 class NotEmptyResponseMiddleware(MiddlewareMixin):
     def process_response(self, _, response):
         if getattr(response, 'content', b'') == b'':
-            response.content = b'{}'
+            try:
+                response.content = b'{}'
+            except AttributeError:
+                pass
         if response.status_code == status.HTTP_204_NO_CONTENT:
             response.status_code = status.HTTP_200_OK
         return response
